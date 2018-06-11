4de270607925c468111c3e6922e73a40af8c623c
wxwdesign <wxwdesign@gmail.com>
2018-06-11 20:08:47 +0800

    add eslint config resources

---
+++ resources/eslint-airbnb-vue/.eslintrc.js
@@ -0,0 +1,55 @@
+// http://eslint.org/docs/user-guide/configuring
+
+module.exports = {
+  root: true,
+  parserOptions: {
+    parser: 'babel-eslint',
+    sourceType: 'module'
+  },
+  env: {
+    browser: true,
+  },
+  extends: ['airbnb-base'],
+  // check if imports actually resolve
+  'settings': {
+    'import/resolver': {
+      'webpack': {
+        'config': 'build/webpack.base.conf.js'
+      }
+    }
+  },
+  // add your custom rules here
+  'rules': {
+    // don't require .vue extension when importing
+    'import/extensions': ['error', 'always', {
+      'js': 'never',
+      'vue': 'never'
+    }],
+    // allow optionalDependencies
+    'import/no-extraneous-dependencies': ['error', {
+      'optionalDependencies': ['test/unit/index.js']
+    }],
+    // allow debugger during development
+    'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0,
+    // allow console during development
+    'no-console': 0,
+    // allow Reassignment of Function Parameters (no-param-reassign)
+    "no-param-reassign": ["error", { "props": false }],
+    // max line length
+    "max-len": ["error", { 
+      "code": 120,
+      "ignoreComments": true,
+      "ignoreTrailingComments": true,
+      "ignoreUrls": true,
+      "ignoreStrings": true,
+      "ignoreTemplateLiterals": true,
+      "ignoreRegExpLiterals": true
+    }],
+    // unused vars
+    "no-unused-vars": ["error", {"args": "none"}],
+    // 嵌套的三元表达式
+    // "no-nested-ternary": 1,
+    // 要求使用一致的 return 语句 
+    "consistent-return": 0,
+  }
+}
