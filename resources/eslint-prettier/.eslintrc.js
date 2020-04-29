0f593387bc2d38c8402c70d3e49bbbc3e54b098c
wxwdesign <wxwdesign@gmail.com>
2020-04-29 18:11:32 +0800

    add eslint-prettier config

---
+++ resources/eslint-prettier/.eslintrc.js
@@ -0,0 +1,23 @@
+module.exports = {
+  env: {
+    commonjs: true,
+    es6: true,
+    node: true,
+  },
+  extends: [
+    'prettier'
+  ],
+  plugins: [
+    'prettier'
+  ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parserOptions: {
+    ecmaVersion: 2020,
+  },
+  rules: {
+    "prettier/prettier": "error"
+  },
+};
