b7743c8892bcbc664238d18f4b8aaf2445318d3b
wxwdesign <wxwdesign@gmail.com>
2021-08-30 17:26:04 +0800

    add eslint-plugin-html to default prettier settings

--- resources/eslint-prettier/.eslintrc.js
+++ resources/eslint-prettier/.eslintrc.js
@@ -4,21 +4,17 @@ module.exports = {
     es6: true,
     node: true,
   },
-  extends: [
-    'prettier'
-  ],
-  plugins: [
-    'prettier'
-  ],
+  extends: ["prettier"],
+  plugins: ["prettier", "html"],
   globals: {
-    Atomics: 'readonly',
-    SharedArrayBuffer: 'readonly',
+    Atomics: "readonly",
+    SharedArrayBuffer: "readonly",
   },
   parserOptions: {
     ecmaVersion: 2020,
     sourceType: "module",
   },
   rules: {
-    "prettier/prettier": "error"
+    "prettier/prettier": "error",
   },
 };
